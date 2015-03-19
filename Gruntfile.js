dbcbff65e54caed5bf1270cb605515d4732acec2
ritz078 <rkritesh078@gmail.com>
2015-03-19 21:44:23 +0530

    commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -322,7 +322,9 @@ module.exports = function (grunt) {
             '*.{ico,png,txt}',
             'images/{,*/}*.webp',
             '{,*/}*.html',
-            'styles/fonts/{,*/}*.*'
+            'styles/fonts/{,*/}*.*',
+            'fonts/*',
+            'font/roboto/*'
           ]
         }, {
           src: 'node_modules/apache-server-configs/dist/.htaccess',
