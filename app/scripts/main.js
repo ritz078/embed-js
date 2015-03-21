c6a3c6c81ac319a22ed5fa48997b284a4bb4e978
ritz078 <rkritesh078@gmail.com>
2015-03-21 19:08:28 +0530

    commit

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -24,4 +24,16 @@ $(document).ready(function(){
       window.location.hash = target;
     });
   });
+  $('.about a[href^="#"]').on('click',function (e) {
+    e.preventDefault();
+
+    var target = this.hash;
+    var $target = $(target);
+
+    $('html, body').stop().animate({
+      'scrollTop': $target.offset().top-60
+    }, 900, 'swing', function () {
+      window.location.hash = target;
+    });
+  });
 });
