f0737ff701392235c8ee61c026c3450455af6faa
ritz078 <rkritesh078@gmail.com>
2015-03-24 16:07:51 +0530

    commit

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,18 +1,9 @@
-$('.element').embedJS();
+"use-strict";
 
-$('.element2').embedJS({
-  gdevAuthKey:'AIzaSyCqFouT8h5DKAbxlrTZmjXEmNBjC69f0ts'
-});
-
-$('.element3').embedJS();
-
-$('.element5').embedJS({
-  audioEmbed:true
-});
 hljs.initHighlightingOnLoad();
 
 $(document).ready(function(){
-  $('ul>a[href^="#"]').on('click',function (e) {
+  $('.nano-content>a[href^="#"]').on('click',function (e) {
     e.preventDefault();
 
     var target = this.hash;
@@ -20,7 +11,7 @@ $(document).ready(function(){
 
     $('html, body').stop().animate({
       'scrollTop': $target.offset().top-60
-    }, 900, 'swing', function () {
+    }, 200, 'swing', function () {
       window.location.hash = target;
     });
   });
@@ -36,4 +27,35 @@ $(document).ready(function(){
       window.location.hash = target;
     });
   });
+  $('ol>li>p a[href^="#"]').on('click',function (e) {
+    e.preventDefault();
+
+    var target = this.hash;
+    var $target = $(target);
+
+    $('html, body').stop().animate({
+      'scrollTop': $target.offset().top-60
+    }, 900, 'swing', function () {
+      window.location.hash = target;
+    });
+  });
+  $('li a[href^="#"]').on('click',function (e) {
+    e.preventDefault();
+
+    var target = this.hash;
+    var $target = $(target);
+
+    $('html, body').stop().animate({
+      'scrollTop': $target.offset().top-60
+    }, 900, 'swing', function () {
+      window.location.hash = target;
+    });
+  });
+});
+$('.collapsible-header').click(function(){
+  setTimeout(
+    function() {
+      $(".nano").nanoScroller();
+    },
+    200);
 });
