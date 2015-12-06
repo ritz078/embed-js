be543cefb8ad99175c4f3dd7382e3d9f2383b412
Ritesh Kumar <rkritesh078@gmail.com>
2015-12-06 16:53:59 +0530

    site completed

--- app/scripts/examples.js
+++
@@ -1,8 +0,0 @@
-$('.left-nav a').click(function(e) {
-    e.preventDefault()
-    var id = $(this).attr('href')
-    $('html,body').animate({
-            scrollTop: $(id).offset().top - 70
-        },
-        'slow');
-})
