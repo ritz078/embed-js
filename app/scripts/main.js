c3b2e600700e3d106ebaf8d39ffc0c5e3f8340f7
Ritesh Kumar <rkritesh078@gmail.com>
2015-12-07 00:04:25 +0530

    bug fixes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,48 +1,61 @@
-class Banner {
-  constructor() {
-    this.strings = [
-      'MARKDOWN',
-      'SOUNDCLOUD.',
-      'MP4 VIDEOS.',
-      'SPOTIFY.',
-      'VIMEO VIDEOS.',
-      'FLICKR IMAGES.',
-      'INSTAGRAM IMAGES.',
-      'TEXT SMILEYS.',
-      'HIGHLIGHTED CODE',
-      'HTML LINKS.',
-      'TWEETS.',
-      'YOUTUBE VIDEOS.',
-      'JSFIDDLE CODE',
-      'CODEPEN CODE',
-      'VINE VIDEOS.',
-      'GITHUB GISTS.',
-      'DAILYMOTION VIDEOS.',
-      'LIVELEAK VIDEOS.',
-      'USTREAM VIDEOS.',
-      'JSBIN CODE',
-      'PLUNKER CODE.',
-      'MP3 SONGS'
-    ];
+var strings = [
+  'MARKDOWN',
+  'SOUNDCLOUD.',
+  'MP4 VIDEOS.',
+  'SPOTIFY.',
+  'VIMEO VIDEOS.',
+  'FLICKR IMAGES.',
+  'INSTAGRAM IMAGES.',
+  'TEXT SMILEYS.',
+  'HIGHLIGHTED CODE',
+  'HTML LINKS.',
+  'TWEETS.',
+  'YOUTUBE VIDEOS.',
+  'JSFIDDLE CODE',
+  'CODEPEN CODE',
+  'VINE VIDEOS.',
+  'GITHUB GISTS.',
+  'DAILYMOTION VIDEOS.',
+  'LIVELEAK VIDEOS.',
+  'USTREAM VIDEOS.',
+  'JSBIN CODE',
+  'PLUNKER CODE.',
+  'MP3 SONGS'
+];
 
-    this.fade();
-  }
 
-  fade() {
-    let i      = 0;
-    let $typed = $('.typed');
-    let self   = this;
-    setInterval(() => {
-        $typed.fadeOut('slow', function () {
-          $(this).html(self.strings[i]).fadeIn();
-        });
-        i === this.strings.length ? i = 0 : i++;
-      },
-      2000
-    )
-    ;
-  }
+function fade() {
+  var i      = 0;
+  var $typed = $('.typed');
+  setInterval(function () {
+    $typed.fadeOut('slow', function () {
+      $(this).html(strings[i]).fadeIn();
+    });
+    i === strings.length ? i = 0 : i++;
+  }, 2000);
 }
 
-var banner = new Banner();
+function isRetina(){
+  var mediaQuery = '(-webkit-min-device-pixel-ratio: 1.5), (min--moz-device-pixel-ratio: 1.5), (-o-min-device-pixel-ratio: 3/2), (min-resolution: 1.5dppx)';
 
+  if (window.devicePixelRatio > 1) {
+    return true;
+  }
+
+  return !!(window.matchMedia && window.matchMedia(mediaQuery).matches);
+}
+
+function lazyload($elem, background, path){
+  var suffix = isRetina() ? '@2x' : '';
+  var url = background ? path.replace('@0.5x',suffix) : $elem.attr('src').replace('@0.5x',suffix);
+  let image = new Image();
+  image.src = url;
+  image.onload = function () {
+    background ? $elem.css('background-image','url('+url+')') : $elem.attr('src',url);
+  };
+}
+
+lazyload($('.browser'));
+lazyload($('.icon'), true, 'images/homepage@0.5x.png');
+
+fade();
