f2aa6125a4f6d75d1d44b49ff2a042191f15097e
Ritesh Kumar <rkritesh078@gmail.com>
2015-12-08 00:30:46 +0530

    responsive

--- scripts/docs.760f9d50.js
+++
@@ -1,1 +0,0 @@
-"use strict";hljs.initHighlightingOnLoad();var data=[];$("h5").each(function(){data.push($(this).html())});var str="";$.each(data,function(){str+='<a href="#'+this+'"><li>'+this+"</li></a>"}),$(".left-nav ul").html(str),$(".left-nav a").click(function(a){a.preventDefault();var b=$(this).attr("href");$("html,body").animate({scrollTop:$(b).offset().top-70},"slow")});
--- scripts/examples.7bb03cd2.js
+++
@@ -1,1 +0,0 @@
-"use strict";$(".left-nav a").click(function(a){a.preventDefault();var b=$(this).attr("href");$("html,body").animate({scrollTop:$(b).offset().top-70},"slow")});
