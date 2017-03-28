00ddd4716023bc0b72f19ccaea2b362bfb3604e8
Ritesh Kumar <ritz078@users.noreply.github.com>
2017-03-28 20:38:16 +0530

    Fix build (#235)
    * build(package): updated packages
    
    * fix(build): path of css in built files
    
    * chore:

--- Gruntfile.js
+++ Gruntfile.js
@@ -95,7 +95,7 @@ module.exports = function (grunt) {
 					style: 'expanded'
 				},
 				files: {
-					'dist/embed.css': 'src/css/embed.scss'
+					'.tmp/embed.css': 'src/css/embed.scss'
 				}
 			}
 		},
@@ -142,18 +142,18 @@ module.exports = function (grunt) {
 		'string-replace': {
 			dist: {
 				files: {
-					'.tmp/embed.css': 'dist/embed.css'
+					'dist/embed.css': '.tmp/embed.css'
 				},
 				options: {
 					replacements: [{
 						pattern: /..\/assets\/images\/loader.svg/g,
-						replacement: 'assets/images'
+						replacement: './assets/images'
 					}, {
 						pattern: /..\/.\/assets\/images\/emojis/g,
-						replacement: 'assets/images/emojis'
+						replacement: './assets/images/emojis'
 					}, {
 						pattern: /..\/assets\/fonts/g,
-						replacement: 'assets/fonts'
+						replacement: './assets/fonts'
 					}]
 				}
 			}
