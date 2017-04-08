72e55e6430ab198823d2c1d40d066122b5cc5200
SnehaMidha <snehamidha32@gmail.com>
2017-04-08 21:41:42 +0530

    correct URL opens in new tab (#242)
    test(facebook): check if correct URL opens in new tab

--- test/e2e/index.js
+++ test/e2e/index.js
@@ -32,8 +32,26 @@ module.exports = {
 			.url(`localhost:${port}/test/facebook`)   // visit the url
 			.waitForElementVisible('.embed-js-applied'); // wait for the body to be rendered
 
+                browser
+                        .click('.ejs-facebook iframe')         // click on the iframe
+                        .windowHandles(function(result) {
+                          this.verify.equal(result.value.length, 2, 'There should be 2 windows open')
+                          var newHandle = result.value[1]
+                          this.switchWindow(newHandle)
+                        })
+                        .assert.urlContains('https://www.facebook.com/madovermarketing/')
+                        .url(function(result){
+                           var link = result.value
+                           let id = link.split('/')
+                           this.verify.equal(id.indexOf('/1402165223182741/') < 0, true)  // id of the post must be same
+                        });
+
 		browser
 			.pause(2000)
+                        .windowHandles(function(result) {
+                          var newHandle = result.value[0]   // switch back
+                          this.switchWindow(newHandle)
+                        })
 			.frame(0)
 			.assert.elementPresent('#facebook') // assert contains
 			.saveScreenshot(conf.imgpath(browser) + 'embed.png')
