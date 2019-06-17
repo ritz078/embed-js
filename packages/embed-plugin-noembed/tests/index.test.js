ca1359b670a92d2bc973bc04299bfbb6f40248cc
ritz078 <rkritesh078@gmail.com>
2019-06-17 14:06:07 +0530

    chore(security): upgrade packages

--- packages/embed-plugin-noembed/tests/index.test.js
+++ packages/embed-plugin-noembed/tests/index.test.js
@@ -1,28 +1,8 @@
 import test from "ava"
-import extend from "just-extend"
-import noembed from "../src"
-import fetch from "isomorphic-unfetch"
 import { isServicePresent } from "../src/noembed-regex"
-
-const options = {
-  replaceUrl: false,
-  inlineEmbed: true,
-  fetch,
-  _embeds: [],
-  _services: []
-}
 
 const text =
   "Nunquam perdere https://www.youtube.com/watch?v=DYsOIjw8Emg https://a.jpg olla https://b.jpg."
-
-test("Plugin: noembed - youtube", async t => {
-  const opts = extend({}, options, {
-    result: text
-  })
-
-  const { result } = await noembed().transform(opts)
-  t.snapshot(result)
-})
 
 test("noembed-regex : isServicePresent", t => {
   t.true(isServicePresent("youtube", text))
